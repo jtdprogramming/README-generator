e879c1600339cdd5b0f083896e488aa578d9f6ad
James D <jtd.programming@gmail.com>
2021-12-27 00:06:07 -0500

    questions complete, package.json description updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 //Resources: https://www.npmjs.com/package/inquirer 
 //           https://nodejs.org/dist/latest-v12.x/docs/api/fs.html#fs_fs_writefile_file_data_options_callback
 //           https://nodejs.org/api/path.html
+//           https://www.tutorialsteacher.com/nodejs/nodejs-module-exports
 
 // import inquirer, fs, path module, markdown-gen 
 const inquirer = require('inquirer');
@@ -19,13 +20,47 @@ const questions = [
   {
     type: "input",
     name: "title",
-    message: "What is the title of your project?",
+    message: "Please enter the title of your project ",
   },
   //Project description
   {
     type: "input",
     name: "description",
     message: "Please enter a brief description of your project: ",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Please select a license",
+    choices: [
+      new inquirer.Separator(),
+      "MIT",
+      new inquirer.Separator(),
+      "Apache-2.0",
+      new inquirer.Separator(),
+      "MPL-2.0",
+      new inquirer.Separator(),
+    ],
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Please enter the 'Test' command ",
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "Please enter the names of contributing parties",
+  },
+  {
+    type: "input",
+    name: "gitHub",
+    message: "Please enter your GitHub username ",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please enter your e-mail ",
   },
 
 ]
